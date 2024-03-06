f2dd517a2627a820cb7a5a0daa1013dd8a4d896c
Shlomo Zippel <shlomo.zippel@gmail.com>
2024-03-06 14:53:31 -0800

    nicer init code

--- pypn5180/__init__.py
+++ pypn5180/__init__.py
@@ -1,7 +1,7 @@
 from . import pypn5180, iso_iec_15693
 
 
-def init(spi, cs, busy):
+def Reader(spi, cs, busy):
     device = pypn5180.PN5180(spi=spi, cs=cs, busy=busy)
     reader = iso_iec_15693.iso_iec_15693(device)
     return reader
